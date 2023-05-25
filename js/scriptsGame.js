aafbde565d8a70d085294ae10d2edc63e72b8772
Vinicius Nascimento Ribeiro <vinicius_eng_mec@outlook.com>
2023-05-25 08:26:00 -0300

    Mario finalizado Git

--- js/scriptsGame.js
+++ js/scriptsGame.js
@@ -1,5 +1,6 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
 
 const jump = () => {
 
@@ -13,16 +14,21 @@ const jump = () => {
 const loop = setInterval(() => {
 
     const pipePosition = pipe.offsetLeft;
+    const cloudsPosition = clouds.offsetLeft;
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     console.log(marioPosition);
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+
         pipe.style.animation = 'none';
-        pipe.style.left = '${pipePosition}px';
+        pipe.style.left = `${pipePosition}px`;
+
+        clouds.style.animation = 'none';
+        clouds.style.left = `${cloudsPosition}px`;
 
         mario.style.animation = 'none';
-        mario.style.bottom = '${marioPosition}px';
+        mario.style.bottom = `${marioPosition}px`;
 
         mario.src = './img/game-over.png';
         mario.style.width = '75px';
